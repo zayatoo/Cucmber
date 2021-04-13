0ae3df2e38a95b5f6a5485aad72b28495beaa933
zayatoo <ahmedzayat2311@gmail.com>
2021-04-13 09:48:56 +0200

    AZ1342021|9:48AM

---
+++ src/test/java/Features/Login.feature
@@ -0,0 +1,7 @@
+Feature: Application Login
+
+  Scenario: Home page default Login
+    Given User is on Landing Page
+    When dfdf
+    Then ddw
+    And gdfgdsf
---
+++ src/test/java/StepDefinations/StepDefination.java
@@ -0,0 +1,16 @@
+package StepDefinations;
+
+
+import io.cucumber.java.en.Given;
+
+public class StepDefination {
+@Given("^User is on Landing Page$")
+    public void User(){
+    //"sdsdsdsd";bndbfbdnfbd
+
+
+}
+
+
+
+}
